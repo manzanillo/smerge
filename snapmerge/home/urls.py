5a7eb6aef9bc7ee55f8c91f80c7f53b6a664c6f6
Tilman Michaeli <tilman.michaeli@fau.de>
2018-02-04 21:54:25 +0100

    prototype for create_project + url fix

--- snapmerge/home/urls.py
+++ snapmerge/home/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.HomeView.as_view(), name='home'),
-    url(r'^(?P<proj_id>[0-9]+)$', views.ProjectView.as_view(), name='proj')
+    url(r'^create_project/$', views.CreateProjectView.as_view(), name='create_proj'),
+    url(r'^(?P<proj_id>[0-9]+)$', views.ProjectView.as_view(), name='proj'),
 
 ]
