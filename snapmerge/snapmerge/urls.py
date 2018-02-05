7aa21e76a5549c4df951cfc5426b61d4a9a7479b
manzanillo <manzanillo>
2018-02-05 22:54:30 +0100

    build custom project layout

--- snapmerge/snapmerge/urls.py
+++ snapmerge/snapmerge/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
 from django.views.generic import RedirectView
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'', include('home.urls')),
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
