47b1e17c0933d6ddf5af5a2a7412de474fc64dd3
manzanillo <manzanillo>
2018-03-08 17:35:51 +0100

    same sprite, different position

--- snapmerge/home/xmltools.py
+++ snapmerge/home/xmltools.py
@@ -73,6 +73,10 @@ def xml_merge(reference_element, subject_element, ref_description='', subject_de
             else:
                 xml_merge(reference_child, subject_child, ref_description, subject_description)
         else:
+            if subject_child.tag == 'sprite' and reference_child.tag == 'sprite' \
+                                             and subject_child.get('name') == reference_child.get('name') \
+                                             and subject_child.get('idx') == reference_child.get('idx'):
+                    xml_merge(reference_child, subject_child, ref_description, subject_description)
             reference_element.append(subject_child)
     return
 
