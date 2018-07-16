beb94a540de88f9634f451f7a7715dcf81d52c96
manzanillo <manzanillo>
2018-07-16 17:46:15 +0200

    drag and drop now works, still missing load and display new node

--- snapmerge/home/urls.py
+++ snapmerge/home/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     url(r'^(?P<proj_id>[0-9]+)$', views.ProjectView.as_view(), name='proj'),
     url(r'^merge/(?P<proj_id>[0-9]+)$', views.MergeView.as_view(), name='merge'),
     url(r'^sync/(?P<proj_id>[0-9]+)$', views.SyncView.as_view(), name='sync'),
-
+    url(r'add/(?P<proj_id>[0-9]+)$',views.AddFileToProjectView.as_view(), name='add')
 ]
