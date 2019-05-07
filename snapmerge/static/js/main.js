d1d444b21b228e515767a29285d576c22743069d
manzanillo <manza@outlook.de>
2019-05-07 10:14:22 +0200

    started switching to npm for js dependencies

---
+++ snapmerge/static/js/main.js
@@ -0,0 +1,22 @@
+import cytoscape from 'cytoscape';
+import jquery from 'jquery';
+import cyqtip from 'cytoscape-qtip';
+import timeago from 'timeago.js';
+import { format, render, cancel, register } from 'timeago.js';
+import dagre from 'cytoscape-dagre';
+
+//import M from 'materialize-css'
+
+cytoscape.use( dagre );
+cyqtip( cytoscape, jquery ); // register extension
+
+
+//console.log(M)
+
+window.cytoscape = cytoscape
+window.timeago = timeago
+window.jQuery = window.$ = jquery
+window.format = format
+//window.Materialize = M
+
+
