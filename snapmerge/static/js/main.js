fa647123bb0391e8689d20ef8a2f57ac2897d404
manzanillo <manza@outlook.de>
2019-05-07 10:17:22 +0200

    switched to npm dependency for cytoscape cxt menu

--- snapmerge/static/js/main.js
+++ snapmerge/static/js/main.js
@@ -4,10 +4,13 @@ import cyqtip from 'cytoscape-qtip';
 import timeago from 'timeago.js';
 import { format, render, cancel, register } from 'timeago.js';
 import dagre from 'cytoscape-dagre';
+import cxtmenu from 'cytoscape-cxtmenu';
+
 
 //import M from 'materialize-css'
 
 cytoscape.use( dagre );
+cytoscape.use( cxtmenu );
 cyqtip( cytoscape, jquery ); // register extension
 
 
