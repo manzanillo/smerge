429c4a9c23c6ac76572d539464ca37b28ac49399
manzanillo <manza@outlook.de>
2019-07-03 13:28:33 +0200

    switched from django.url.path to django.conf.urls.url for url patterns

--- snapmerge/config/urls.py
+++ snapmerge/config/urls.py
@@ -14,14 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.conf.urls import url, include
-from django.views.generic import RedirectView
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
     url(r'', include('home.urls')),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
