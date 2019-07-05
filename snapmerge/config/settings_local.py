092a69b136de00b0360f848f43cbdd63b6e22b04
manzanillo <manza@outlook.de>
2019-07-05 10:24:53 +0200

    autoreconnect to websocket after connection got lost

--- snapmerge/config/settings_local.py
+++ snapmerge/config/settings_local.py
@@ -6,7 +6,7 @@ SECRET_KEY = '()fvd?-m+=quyxz*_3v+gjg!d)8n0(wbo*k)(0kwtwuryr4nil'
 DEBUG = True
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-COMPRESS_OFFLINE = True
+#COMPRESS_OFFLINE = True
 
 ASGI_APPLICATION = "routing.application"
 CHANNEL_LAYERS = {
--- snapmerge/static/js/websocket.js
+++ snapmerge/static/js/websocket.js
@@ -1,83 +1,89 @@
 var projectRoom = document.querySelector(".project-heading").getAttribute("data-proj-id");
 
-var websocketProtocol = location.protocol === 'https:' ? 'wss://' : 'ws://'
 
-var socket = new WebSocket(
-    websocketProtocol + window.location.host +
-    '/ws/' + projectRoom + '/');
+var connect = function(){
 
-socket.onmessage = function(e) {
-    var data = JSON.parse(e.data);
-    var event = data['event']
-    var new_node = data['node'];
+    var websocketProtocol = location.protocol === 'https:' ? 'wss://' : 'ws://'
 
-    window.cy.add({
-                    group: "nodes",
-                    data: {
-                        id: new_node.id,
-                        href: new_node.file_url,
-                        description: new_node.description,
-                        timestamp : new_node.timestamp,
-                        number_scripts : new_node.number_scripts,
-                        number_sprites : new_node.number_sprites,
-                        ancestors : new_node.ancestors,
-                        color: new_node.color,
-                    }
+    var socket = new WebSocket(
+        websocketProtocol + window.location.host +
+        '/ws/' + projectRoom + '/');
+
+    socket.onmessage = function(e) {
+        var data = JSON.parse(e.data);
+        var event = data['event']
+        var new_node = data['node'];
+
+        window.cy.add({
+                        group: "nodes",
+                        data: {
+                            id: new_node.id,
+                            href: new_node.file_url,
+                            description: new_node.description,
+                            timestamp : new_node.timestamp,
+                            number_scripts : new_node.number_scripts,
+                            number_sprites : new_node.number_sprites,
+                            ancestors : new_node.ancestors,
+                            color: new_node.color,
+                        }
+                    });
+        if (new_node.ancestors){
+            // add new edges for the new node
+            new_node.ancestors.forEach(function(ancestorId){
+                cy.add ({
+                    group: "edges",
+                    data: { source: ancestorId, target: new_node.id }
                 });
-    if (new_node.ancestors){
-        // add new edges for the new node
-        new_node.ancestors.forEach(function(ancestorId){
-            cy.add ({
-                group: "edges",
-                data: { source: ancestorId, target: new_node.id }
             });
-        });
-        // refresh the layout of the nodes
-        window.cy.layout({name: 'dagre'}).run()
+            // refresh the layout of the nodes
+            window.cy.layout({name: 'dagre'}).run()
+        }
+
+
+        /*if (false){
+
+            new_node.ancestors
+            collection = collection.union(window.cy.getElementById());
+
+
+            var positions = findOptimalPosition(selectedElements);
+
+            var eleNeu = cy.add({
+                group: "nodes",
+                data: {
+                    id: new_node.id,
+                    href: new_node.file_url,
+                    description: new_node.description,
+                    timestamp : new_node.timestamp,
+                    number_scripts : new_node.number_scripts,
+                    number_sprites : new_node.number_sprites,
+                    ancestors : new_node.ancestors,
+                    color: new_node.color,
+                },
+                position: { x: positions[0], y: positions[1] }
+            });
+
+            selectedElements.forEach(function(ele){
+               cy.add ({
+                    group: "edges",
+                    data: { source: ele.data('id'), target: new_node.id }
+                });
+            });
+        } */
+    };
+
+    socket.onclose = function(e) {
+        console.error('Chat socket closed unexpectedly');
+        setTimeout(connect, 1000);
+    };
+
+    socket.onopen = function(e){
+        console.log('websocket connection established')
     }
 
-
-    /*if (false){
-
-        new_node.ancestors
-        collection = collection.union(window.cy.getElementById());
-
-
-        var positions = findOptimalPosition(selectedElements);
-
-        var eleNeu = cy.add({
-            group: "nodes",
-            data: {
-                id: new_node.id,
-                href: new_node.file_url,
-                description: new_node.description,
-                timestamp : new_node.timestamp,
-                number_scripts : new_node.number_scripts,
-                number_sprites : new_node.number_sprites,
-                ancestors : new_node.ancestors,
-                color: new_node.color,
-            },
-            position: { x: positions[0], y: positions[1] }
-        });
-
-        selectedElements.forEach(function(ele){
-           cy.add ({
-                group: "edges",
-                data: { source: ele.data('id'), target: new_node.id }
-            });
-        });
-    } */
-};
-
-socket.onclose = function(e) {
-    console.error('Chat socket closed unexpectedly');
-};
-
-socket.onopen = function(e){
-    console.log('websocket connection established')
 }
 
-
+connect();
 
 var findOptimalPosition = function(elements){
     var xPositions = [];
