0a89153ead7b257f97f60d495eb0e33ef9dd54e4
manzanillo <manza@outlook.de>
2019-07-05 14:05:15 +0200

    move sqlite-database to subfolder

--- snapmerge/config/settings_base.py
+++ snapmerge/config/settings_base.py
@@ -103,7 +103,7 @@ CORS_ORIGIN_WHITELIST = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'database/db.sqlite3'),
     }
 }
 
