2620a311482b793de55aa2945a87eaa44f422f68
manzanillo <manza@outlook.de>
2019-07-05 16:54:59 +0200

    updated docker settings for server

--- snapmerge/config/settings_docker.py
+++ snapmerge/config/settings_docker.py
@@ -1,6 +1,14 @@
 from .settings_base import *
 
-URL = 'http://127.0.0.1:80'
+#URL = 'http://127.0.0.1:80'
+URL = 'https://smerge.org'
+
+SECRET_PATH = '/var/secrets/smerge/secrets.smerge.json'
+
+secret_file = open(SECRET_PATH).read()
+secrets = json.loads(secret_file)
+SECRET_KEY = secrets["SECRET_KEY"]
+EMAIL_HOST_PASSWORD = secrets["EMAIL_HOST_PASSWORD"]
 
 SECRET_KEY = '()fvd?-m+=quyxz*_3v+gjg!d)8n0(wbo*k)(0kwtwuryr4nil'
 DEBUG = True
@@ -19,3 +27,11 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
+
+
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+X_FRAME_OPTIONS = 'DENY'
