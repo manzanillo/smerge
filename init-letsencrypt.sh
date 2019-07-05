6dfe5d57f76c065cb9ae99d38345ff207dfd3d79
manzanillo <manza@outlook.de>
2019-07-05 14:47:44 +0200

    added email adress

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains=(smerge.org www.smerge.org)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
+email="ddi-smerge@fau.de" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
