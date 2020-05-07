0fb1c4a37cafc925a31f71eb1bb5ea321c3012f6
manzanillo <manza@outlook.de>
2020-05-07 12:50:24 +0200

    update local settings

--- snapmerge/config/settings_local.py
+++ snapmerge/config/settings_local.py
@@ -5,7 +5,7 @@ URL = 'http://127.0.0.1:8000'
 SECRET_KEY = '()fvd?-m+=quyxz*_3v+gjg!d)8n0(wbo*k)(0kwtwuryr4nil'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-#COMPRESS_OFFLINE = True
+COMPRESS_OFFLINE = True
 
 ASGI_APPLICATION = "routing.application"
 CHANNEL_LAYERS = {
@@ -16,3 +16,7 @@ CHANNEL_LAYERS = {
         },
     },
 }
+
+ALLOWED_HOSTS = ['127.0.0.1', 'faui20s.cs.fau.de', 'faui20s.informatik.uni-erlangen.de', 'smerge.org']
+
+DEBUG = True
