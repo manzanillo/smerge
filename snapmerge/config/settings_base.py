0fe7748550d7bd1c84f4415af13adbc7562b344f
Stefan <manza@outlook.de>
2020-09-25 13:46:42 +0200

    added compressor toolkit again

--- snapmerge/config/settings_base.py
+++ snapmerge/config/settings_base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'channels',
     'home',
     'compressor',
+    'compressor_toolkit',
 ]
 
 MIDDLEWARE = [
@@ -181,6 +182,11 @@ COMPRESS_CSS_FILTERS = [
 COMPRESS_JS_FILTERS = [
     'compressor.filters.jsmin.JSMinFilter',
 ]
+
+COMPRESS_PRECOMPILERS = (
+    ('module', 'compressor_toolkit.precompilers.ES6Compiler'),
+    ('css', 'compressor_toolkit.precompilers.SCSSCompiler'),
+)
 
 COMPRESS_ENABLED = True
 
