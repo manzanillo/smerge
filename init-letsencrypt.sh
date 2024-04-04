9c5f9831227b3178f18264cae1e7a90831b00d27
Richie <richieschmid@live.de>
2024-04-04 21:38:45 +0200

    modified cert parts

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,10 +5,10 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(smerge.org www.smerge.org)
+domains=(thorstest.hopto.org)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="ddi-smerge@fau.de" # Adding a valid address is strongly recommended
+email="richieschmid@live.de" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
