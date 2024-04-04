e198ab977f3b3e57735c6137f7c23978aeabdda1
Richie <richieschmid@live.de>
2024-04-04 18:40:39 +0200

    tmp daphne config change for diff. server test

--- snapmerge/config/settings_daphne.py
+++ snapmerge/config/settings_daphne.py
@@ -1,20 +1,21 @@
 from .settings_base import *
 import json
 
-URL = 'http://0.0.0.0:8000'
+URL = "http://0.0.0.0:8000"
 # POST_BACK_URL = 'https://rs-kubuntu.local'
-POST_BACK_URL = 'https://idpsmerge.duckdns.org'
+# POST_BACK_URL = 'https://idpsmerge.duckdns.org'
+POST_BACK_URL = "https://thorstest.hopto.org"
 
-SECRET_KEY = '()fvd?-m+=quyxz*_3v+gjg!d)8n0(wbo*k)(0kwtwuryr4nil'
-#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+SECRET_KEY = "()fvd?-m+=quyxz*_3v+gjg!d)8n0(wbo*k)(0kwtwuryr4nil"
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 COMPRESS_OFFLINE = False
 
 # allow cross for testing...
-#CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 CORS_ORIGIN_ALLOW_ALL = True
-SECURE_REFERRER_POLICY = 'unsafe-url'
+SECURE_REFERRER_POLICY = "unsafe-url"
 CORS_ALLOW_HEADERS = "*"
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
@@ -32,23 +33,44 @@ CHANNEL_LAYERS = {
 }
 
 CORS_ORIGIN_WHITELIST = CORS_ORIGIN_WHITELIST + [
-    'https://rs-kubuntu.local', 'https://air.local', "https://idpsmerge.duckdns.org"
+    "https://rs-kubuntu.local",
+    "https://air.local",
+    "https://idpsmerge.duckdns.org",
+    "https://thorstest.hopto.org",
 ]
 
-CSRF_TRUSTED_ORIGINS = CSRF_TRUSTED_ORIGINS + ['https://rs-kubuntu.local' , "https://idpsmerge.duckdns.org"] + ['https://air.local']
+CSRF_TRUSTED_ORIGINS = (
+    CSRF_TRUSTED_ORIGINS
+    + [
+        "https://rs-kubuntu.local",
+        "https://idpsmerge.duckdns.org",
+        "https://thorstest.hopto.org",
+    ]
+    + ["https://air.local"]
+)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'faui20s.cs.fau.de', 'faui20s.informatik.uni-erlangen.de', 'smerge.org', 'idpsmerge.duckdns.org', 'smerge_server', 'rs-kubuntu.local', 'air.local']
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "faui20s.cs.fau.de",
+    "faui20s.informatik.uni-erlangen.de",
+    "smerge.org",
+    "idpsmerge.duckdns.org",
+    "smerge_server",
+    "rs-kubuntu.local",
+    "air.local",
+    "thorstest.hopto.org",
+]
 
 DEBUG = False
 BETA = True
 
-SECRET_PATH = '../secrets/smerge/secrets.smerge.json'
+SECRET_PATH = "../secrets/smerge/secrets.smerge.json"
 secret_file = open(SECRET_PATH).read()
 secrets = json.loads(secret_file)
 EMAIL_HOST_PASSWORD = secrets["EMAIL_HOST_PASSWORD"]
 EMAIL_SENDER = "idppi@idpsmerge.duckdns.org"
 
-EMAIL_HOST = 'in-v3.mailjet.com'
+EMAIL_HOST = "in-v3.mailjet.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = secrets["EMAIL_API_KEY"]
 
